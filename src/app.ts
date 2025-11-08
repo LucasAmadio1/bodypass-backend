3dc4e699637f97cfd4cef5b6f55428e912cf1a6d
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-11-08 17:26:05 -0300

    fix

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'BodyPass API',
+      description: 'App for gym check-ins and workout attendance tracking.',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs',
+})
---
+++ src/env.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
+  HOST: z.string().default('0.0.0.0'),
+  DATABASE_URL: z.url(),
+})
+
+export const env = envSchema.parse(process.env)
